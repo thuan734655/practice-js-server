88ba9ac0f3bf63c17fb20e28ff3fc865f0eb91c2
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-08-21 01:13:39 +0700

    update router user

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,34 +1,64 @@
 import express from "express";
+import bodyParser from "body-parser";
 import connectDB from "../ConnectDB/connect.js";
 
 const router = express.Router();
 
-// get all user
-router.get("/all", (req,res) => {
-    connectDB.query("select * from user", (err, result) => {
-        if(err) {
-            res.status(500).send("get infomation of user fail");
-            return;
-        }
-        res.json(result);
-    });
+router.use(bodyParser.json());
+
+// Get all users
+router.get("/all", (req, res) => {
+  connectDB.query("SELECT * FROM user", (err, result) => {
+    if (err) {
+      res.status(500).send("Failed to get user information");
+      return;
+    }
+    res.json(result);
+  });
 });
 
-//get user by id
-router.get("/id", (req,res) => {
-    const {id} = req;
+// Get user by ID
+router.get("/id", (req, res) => {
+  const { id } = req.query;
 
-    if (!id) {
-        return res.status(400).send('id invalid');
+  if (!id) {
+    return res.status(400).send("ID invalid");
+  }
+
+  connectDB.query(
+    "SELECT * FROM user WHERE idUser = ?",
+    [id],
+    (err, result) => {
+      if (err) {
+        res.status(400).send("Failed to get user information by ID");
+        return;
+      }
+      res.json(result);
     }
+  );
+});
 
-    connectDB.query("select * from user where idUser = ? ",[id],(err, result) => {
-        if(err) {
-            res.status(500).send("get infomation user by id fail");
-            return;
-        }
-        res.json(result);
-    });
+// Check login
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  if (!email || !password) {
+    return res.status(400).send("Invalid data");
+  }
+
+  connectDB.query(
+    "SELECT idUser FROM `user` WHERE email = ? and password = ?",
+    [email, password],
+    (err, result) => {
+      if (err) {
+        return res.status(500).send("Login failed");
+      }
+      if (result.length === 0) {
+        return res.status(404).send("Account not found");
+      }
+      return res.status(200).send("Login successful");
+    }
+  );
 });
 
 export default router;
