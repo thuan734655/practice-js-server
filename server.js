a1663aa7456ccbb3e15584e12bc251647c3ae7f3
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-08-21 01:42:32 +0700

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import userRouter from "./router/userRouter.js";
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+app.use(cors({
+     origin: '*', 
+   }));
 
 app.use('/user', userRouter);
 
