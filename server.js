041fafa0b5663948959a7d8de8de4803f6c8b770
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-11-02 17:07:06 +0700

    fix baseURl

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRouter from "./routes/authRoutes.js";
 import routerMovie from "./routes/moviesRouter.js";
 import routerVideo from "./routes/videoRoutes.js";
 import routerTvShows from "./routes/TVShowsRoutes.js";
+
 const app = express();
 const port = 3000;
 
@@ -16,23 +17,25 @@ const __dirname = path.dirname(__filename);
 // Middleware
 app.use(
   cors({
-    origin: "https://practice-js1.onrender.com",
+    origin: ["http://localhost:1234", "https://practice-js1.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "baseURL"],
-    credentials: true,
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   })
 );
 
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/images", express.static(path.join(__dirname, "images"))); // Serve static files
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json()); // Parse JSON requests
+app.use(cookieParser()); // Parse cookies from requests
 
+// Define routes
 app.use("/", authRouter);
 app.use("/", routerMovie);
 app.use("/", routerVideo);
 app.use("/", routerTvShows);
 
+// Start the server
 app.listen(port, () => {
-  console.log("server listen port 3000");
+  console.log(`Server is listening on port ${port}`);
 });
